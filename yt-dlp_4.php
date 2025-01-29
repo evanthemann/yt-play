869626cf3bf15e3888e63fd28584b992d221c5b3
Evan <evanmann92@gmail.com>
2025-01-28 20:13:19 -0500

    trying add doctype to top

--- yt-dlp_4.php
+++ yt-dlp_4.php
@@ -1,10 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php
 
     // Check if url provided
 
     $url = isset($_GET['url']) ? $_GET['url'] : '';
-
-    $output = '';
 
     if ($url) {
         
@@ -23,8 +23,7 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
