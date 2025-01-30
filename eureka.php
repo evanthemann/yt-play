887be7fc0c5c35e03165abaa1a00936c66fec11b
Evan <evanmann92@gmail.com>
2025-01-29 19:40:53 -0500

    add items

--- eureka.php
+++ eureka.php
@@ -22,7 +22,10 @@
     <h2>Enter URL and formats to download</h2>
     <form action="eureka_4_download.php" method="post">
         <label for="url">YouTube URL:</label><br>
+        <input type="text" id="url" name="url" required><br>
+        <label for="url">Format 1:</label><br>
         <input type="text" id="fmt1" name="fmt1" required><br>
+        <label for="url">Format 2:</label><br>
         <input type="text" id="fmt2" name="fmt2" required><br>
         <button type="submit">Download</button>
     </form>
